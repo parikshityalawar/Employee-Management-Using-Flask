83e9cd5ed77280b9298e6979fd68f85f384ec90b
Parikshit Yalawar <148613058+parikshityalawar@users.noreply.github.com>
2023-10-21 13:00:23 +0530

    App File Added

---
+++ App.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+# Database configration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/empflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Initialization and creation of tables in database
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+
+@app.route('/')
+def Index():
+    all_data = Data.query.all()
+
+    return render_template("index.html", employees=all_data)
+
+
+# Add Employee into Database
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        # Flash message over screen for confirmation Check Position in Html file index.html above <table> to show.
+        flash("New Employee Inserted Successfully.")
+
+        return redirect(url_for('Index'))
+
+
+# update the data into database
+# route is specified into the action field into the html file
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('Index'))
+
+
+
+@app.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+
+    flash("Employee deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,23 @@
+# Employee-Management-System-Using-Flask-CRUD-
+
+Environment: Flask App <br>
+database Used: Localhost Xampp server</br>
+database Name: empflask</br>
+(Just Create Database with database name and you are good to go)</br></br>
+
+<h2>Screenshots</h2>
+<div>
+    <p>
+      <img src="img/Screenshot (7).png" width="400">
+      <img src="img/Screenshot (8).png" width="400">
+      </p><br>
+      <p>
+      <img src="img/Screenshot (9).png" width="400">
+      <img src="img/Screenshot (10).png" width="400">
+      </p>
+      <br>
+      <p>
+      <img src="img/Screenshot (11).png" width="400">
+      <img src="img/Screenshot (12).png" width="400">
+      </p>
+</div>
